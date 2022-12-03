86c69880f5f9d52703d1e510364539a4bedf8ec0
ako-tech <akotech20@gmail.com>
2022-12-03 13:50:12 +0100

    basic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,44 @@
+import {
+    fromEvent,
+    map,
+    Observable,
+    of,
+    startWith,
+    switchMap,
+    takeWhile,
+    tap,
+    timer,
+    withLatestFrom,
+} from "rxjs";
 import "./style.css";
+
+const count = document.getElementById("count") as HTMLElement;
+const seconds = document.getElementById("seconds") as HTMLInputElement;
+const toggle = document.getElementById("start-stop") as HTMLButtonElement;
+
+const toggleClick = fromEvent(toggle, "click");
+const secondsValueChanges = fromEvent(seconds, "input").pipe(
+    map(({ target }) => (target as HTMLInputElement).value),
+    startWith(seconds.value)
+);
+
+const generateCountDown = (lengthInSeconds: number): Observable<number> => {
+    const step = 1000;
+
+    return of(lengthInSeconds).pipe(
+        switchMap((seconds) =>
+            timer(0, step).pipe(
+                map((tick) => seconds - tick),
+                takeWhile((count) => count >= 0)
+            )
+        )
+    );
+};
+
+toggleClick
+    .pipe(
+        withLatestFrom(secondsValueChanges, (_, seconds) => Number(seconds)),
+        switchMap((seconds) => generateCountDown(seconds)),
+        tap((currentCount) => (count.textContent = `${currentCount}`))
+    )
+    .subscribe();
