cdf51c22c35d87e82b317da2cae1a595004778c6
ako-tech <akotech20@gmail.com>
2022-12-07 19:17:01 +0100

    final

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { fromEvent, tap, timer } from "rxjs";
+import { fromEvent, tap, timer, withLatestFrom } from "rxjs";
 import { renderCount } from "./renderer";
 import "./style.css";
 
 const captureBtn = document.getElementById("capture-btn") as HTMLButtonElement;
 
 const captureClick$ = fromEvent(captureBtn, "click");
-const counter$ = timer(0, 1000).pipe(tap(renderCount));
+const counter$ = timer(5000, 1000).pipe(tap(renderCount));
+
+captureClick$
+    .pipe(withLatestFrom(counter$, (click, count) => count))
+    .subscribe(console.log);
