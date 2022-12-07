8cfe16e45f6ac110e6fd72d7a7eb1f3bd3984b3e
ako-tech <akotech20@gmail.com>
2022-12-07 11:37:55 +0100

    revert for video

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { fromEvent, map, tap, timer, withLatestFrom } from "rxjs";
+import { fromEvent, tap, timer } from "rxjs";
 import { renderCount } from "./renderer";
 import "./style.css";
 
@@ -6,10 +6,3 @@ const logBtn = document.getElementById("log-button") as HTMLButtonElement;
 
 const logClick$ = fromEvent(logBtn, "click");
 const counter$ = timer(0, 1000).pipe(tap(renderCount));
-
-logClick$
-    .pipe(
-        withLatestFrom(counter$, (event, count) => count)
-        // map(([event, count]) => count)
-    )
-    .subscribe(console.log);
